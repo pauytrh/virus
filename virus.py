431fb53b7e918b2c641884f118151cd2acc3b79d
pauytrh <ari.lei+github@hanisui.school>
2024-10-10 19:05:48 +0300

    made the virus "better"...

--- virus.py
+++ virus.py
@@ -1,9 +1,5 @@
-import os,random,mouse
-I=10
-while I>1:
-    os.system(f"mkdir {random.randint(1, 1000000)}")
+import random
+import mouse
+while True:
     mouse.move(random.randint(-683, 683), random.randint(-384, 384), False)
     mouse.double_click()
-    os.system("start")
-    os.system("calc")
-    I-=1
